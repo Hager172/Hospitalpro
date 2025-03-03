0ccb8b1143afcb8af682bcbddeed3b69461fb3f0
fatma <fatmaabdelhamid909@gmail.com>
2025-03-04 00:51:03 +0200

    add fatma class

--- first/obj/Debug/net9.0-windows/first.AssemblyInfo.cs
+++ first/obj/Debug/net9.0-windows/first.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c6345fe0d4589be10141e5fa497a3a61ce3f35")]
 [assembly: System.Reflection.AssemblyProductAttribute("first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
