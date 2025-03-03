7f7cd189d2330d7c40be7b724fb904595ea4cbe2
Alaa Elsisy <elsisyalaa0@gmail.com>
2025-03-04 00:59:48 +0200

    alaa1

---
+++ first/models/Class122.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first.models
+{
+    public class Class122
+    {
+        public int DoctorId { get; set; }
+
+     
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
